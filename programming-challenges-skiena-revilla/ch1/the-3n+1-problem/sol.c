e00ee8766b9e5b239be108b453b0b0c5d70a1171
Dias Culenga Daniel <diasculengadaniel908@gmail.com>
2025-10-19 17:38:04 +0100

    feat:Implement the conjecture() and max_cycle() function to c lang solution of the 3n+1 problem

---
+++ programming-challenges-skiena-revilla/ch1/the-3n+1-problem/sol.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int conjecture(int n){
+ int count = 1;//Because the edges counts too.
+ while(n>1){
+  if(n%2==0)
+   n= n/2;
+  else
+   n= n*3+1;
+  count++;
+ } 
+ return count;
+}
+
+void  max_cycle(int i, int j){
+ int cycle = 0;
+ int max = 0;
+ int init_i = i;
+ while(i<=j){
+  cycle = conjecture(i++); 
+  if(cycle>max)
+   max = cycle;
+ }
+printf("%d %d %d\n",init_i,j,max); 
+}
+
+void main(void){
+ max_cycle(1,10);
+}
